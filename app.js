7097dcd18a99fe0733dce36c704214c2d509a86b
Arab45 <horlakunleolayiwola@gmail.com>
2025-04-26 15:09:46 -0700

    add user authentication and email handling features; implement user model, validation, and middleware

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const { connectToDB } = require('./src/db');
 const app = express();
+require("dotenv").config();
+const cors = require('cors');
+const userRouter = require('./src/router/user-router');
 
-app.get('/', (req, res) => {
-  res.send('Hello Express!');
-});
+
+app.use(express.json());
+app.use('/create', userRouter);
+
 
 app.listen(3000, () => {
   console.log('Server running on http://localhost:3000');
+  connectToDB();
 });
---
+++ src/router/user-router.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+const { signUp, fetchAllUser, fetchSingleUser } = require("../controller/user-controller");
+const { validateSignUp, validation } = require('../middleware/validator');
+const { sendUserEmail, userSessionEmail, userResetPasswordEmail, userEmailPasswordSuccess, userTokenEmail } = require("../../service/emailTemp");
+const { userExistence } = require("../middleware/user");
+const { loginProcess, loginIn, verifyLoginUserToken, logOut, forgetPasswordToken, resetPassword } = require("../controller/userAuth-Controller");
+
+router.post("/sign-up", validateSignUp, validation, userExistence, signUp, sendUserEmail);
+router.get('/allUser', fetchAllUser);
+router.get('/singleUser/:id', fetchSingleUser);
+router.post('/signIn', loginProcess, loginIn);
+router.get('/verifyLogin', verifyLoginUserToken, userSessionEmail);
+router.get('/logout', logOut);
+router.post('/forgetPassword', forgetPasswordToken, userResetPasswordEmail);
+router.post('/reset-password/:token', resetPassword, userEmailPasswordSuccess);
+
+
+module.exports = router;
